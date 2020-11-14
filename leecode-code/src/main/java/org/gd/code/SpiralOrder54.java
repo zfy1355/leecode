3d43d05fc061bc615eb0bb1be4c5ea8e802f5f0b
zhangfengyang <W>
2020-11-14 23:38:43 +0800

    spiralOrder 54

---
+++ leecode-code/src/main/java/org/gd/code/SpiralOrder54.java
@@ -0,0 +1,49 @@
+package org.gd.code;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 4, 5, 6 ],
+ *  [ 7, 8, 9 ]
+ * ]
+ * 输出: [1,2,3,6,9,8,7,4,5]
+ * 示例 2:
+ *
+ * 输入:
+ * [
+ *   [1, 2, 3, 4],
+ *   [5, 6, 7, 8],
+ *   [9,10,11,12]
+ * ]
+ * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/spiral-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SpiralOrder54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int x = 0;
+        int y = 0;
+        List<Integer> data = new ArrayList<>();
+        return spiralOrder(matrix,m,n,x,y,data);
+        
+    }
+
+    private List<Integer> spiralOrder(int[][] matrix, int m, int n, int x, int y, List<Integer> data) {
+
+        return data;
+    }
+
+}
