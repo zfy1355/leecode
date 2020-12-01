d7ce782772d1afc8f9bf9546f69a07c8e8fcf3e2
zhangfengyang <zhangfengyang@jd.com>
2020-12-01 20:16:48 +0800

    两数和

--- leecode-code/src/main/java/org/gd/code/TowNumSum.java
+++ leecode-code/src/main/java/org/gd/code/TowNumSum.java
@@ -1,5 +1,8 @@
 package org.gd.code;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  *
@@ -19,4 +22,18 @@ package org.gd.code;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class TowNumSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> data = new HashMap();
+        int[] res = new int[2];
+        for(int i=0;i<nums.length;i++){
+            int key = target - nums[i];
+            if(data.get(key) ==null){
+                data.put(nums[i],i);
+            }else{
+                res[0] = data.get(key);
+                res[1] = i;
+            }
+        }
+        return res;
+    }
 }
