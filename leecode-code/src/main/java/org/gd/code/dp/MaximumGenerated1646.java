78e1cab427f43cb1220eb4950609b20a50fad3d0
zfy1355 <zfy1355@163.com>
2023-03-01 20:36:34 +0800

    add：泰波那契序列 & 获取生成数组中的最大值

---
+++ leecode-code/src/main/java/org/gd/code/dp/MaximumGenerated1646.java
@@ -0,0 +1,79 @@
+package org.gd.code.dp;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 1646. 获取生成数组中的最大值
+ * 简单
+ * 75
+ * 相关企业
+ * 给你一个整数 n 。按下述规则生成一个长度为 n + 1 的数组 nums ：
+ *
+ * nums[0] = 0
+ * nums[1] = 1
+ * 当 2 <= 2 * i <= n 时，nums[2 * i] = nums[i]
+ * 当 2 <= 2 * i + 1 <= n 时，nums[2 * i + 1] = nums[i] + nums[i + 1]
+ * 返回生成数组 nums 中的 最大 值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 7
+ * 输出：3
+ * 解释：根据规则：
+ *   nums[0] = 0
+ *   nums[1] = 1
+ *   nums[(1 * 2) = 2] = nums[1] = 1
+ *   nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
+ *   nums[(2 * 2) = 4] = nums[2] = 1
+ *   nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
+ *   nums[(3 * 2) = 6] = nums[3] = 2
+ *   nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
+ * 因此，nums = [0,1,1,2,1,3,2,3]，最大值 3
+ * 示例 2：
+ *
+ * 输入：n = 2
+ * 输出：1
+ * 解释：根据规则，nums[0]、nums[1] 和 nums[2] 之中的最大值是 1
+ * 示例 3：
+ *
+ * 输入：n = 3
+ * 输出：2
+ * 解释：根据规则，nums[0]、nums[1]、nums[2] 和 nums[3] 之中的最大值是 2
+ *
+ *
+ * 提示：
+ *
+ * 0 <= n <= 100
+ */
+public class MaximumGenerated1646 {
+    public int getMaximumGenerated(int n){
+        if(n<2){
+            return n;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        int max = dp[1];
+        for(int i=1;i<= n;i++) {
+            if(i%2==0){
+                dp[i] = dp[i/2];
+            }else{
+                dp[i] = dp[i/2] + dp[i/2+1];
+            }
+            max = getMax(max , dp[i]);
+        }
+        System.out.println(JSON.toJSONString(dp));
+        return  max;
+    }
+
+    public int getMax(int a,int b){
+        return a>b?a:b;
+    }
+
+    public static void main(String[] args) {
+        MaximumGenerated1646 maximumGenerated1646 = new MaximumGenerated1646();
+        System.out.println(maximumGenerated1646.getMaximumGenerated(100));
+    }
+}
